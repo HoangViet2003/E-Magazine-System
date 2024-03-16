a19454712eacfdfc52ff4fe6665faa5aa599cb8c
HoangViet2003 <nguyenhoangviet10d1@gmail.com>
2024-03-16 16:13:41 +0700

    implement get all contribution for marketing coodinator

--- server/src/controllers/ContributionController.js
+++ server/src/controllers/ContributionController.js
@@ -1,4 +1,6 @@
-const {Contribution} = require('../models');
+const {Contribution,Article,Faculty} = require('../models');
+const { catchAsync } = require("../utils");
+
 
 const createContribution = async (req, res) => {
 	try {
@@ -46,6 +48,40 @@ const getAllContributions = async (req, res) => {
 		res.status(500).json({ error: error.message });
 	}
 };
+
+const getAllContributionByMarketingCoordinator = catchAsync(async (req, res) => {
+	try{
+		const {facultyId} = req.body;
+
+		//check if faculty exists and falcutyId is valid
+
+		const marketingCoordinator = req.user;
+		const faculty = await Faculty.findOne({_id: faculty});
+		if(!faculty){
+			return res.status(400).json({error: "Faculty does not exist"});
+		}
+
+		//check marketing coordinator is the marketing coordinator of the faculty
+		if(faculty.marketingCoordinatorId !== marketingCoordinator._id){
+			return res.status(403).json({error: "You are not the marketing coordinator of this faculty"});
+		}
+
+		
+		const contributions = await Contribution.find({facultyId: facultyId});
+
+
+
+		res.status(200).json({
+			status: "success",
+			contributions,
+		});
+	}catch(error){
+		res.status(500).json({ error: error.message });
+	}
+});
+
+
+
 
 module.exports = {
     createContribution,
